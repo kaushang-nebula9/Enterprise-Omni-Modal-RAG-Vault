8544d45c402611aabf77cc276af31b0014b399ce
Kaushang Surya <kaushangsurya29@gmail.com>
2026-07-01 10:09:30 +0530

    Feat: Run Alembic migrations automatically on startup

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,12 @@
 import os
 import uvicorn
+from alembic.config import Config
+from alembic import command
 
 if __name__ == "__main__":
+    # Run migrations on startup
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
